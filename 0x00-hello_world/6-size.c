6f964d46426bc68b9af5d22b35d284458f0453ce
judith <judithuchechi62@gmail.com>
2022-09-11 19:12:34 -0700

    Byte size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,4 +19,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(v));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(d));
 	return (0);
+}
 
