1efed280c1820f9c614f59c784f4efe8ea0545b7
judith <judithuchechi62@gmail.com>
2022-09-11 17:25:30 -0700

    Print Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *h = "\"Programming is like building a multilingual puzzle";
+	puts(h);
+	return (0);
+}
