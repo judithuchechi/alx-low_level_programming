f5e905233bf0ac4a3dd4c98c4c0c562f682e0f01
judith <judithuchechi62@gmail.com>
2022-09-13 20:00:14 -0700

    NewAlphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * print_alphabet: prints a-z  followed by new line using the _putchar function
+ * print_alphabet - prints a-z  followed by new line using the _putchar function
  *
  */
 void print_alphabet(void)
