10c557b84f282a74aaaf63489cc3bffae25171bf
judith <judithuchechi62@gmail.com>
2022-09-15 05:34:24 -0700

    Positive or Negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Decription - print value of input as zero, negative or positive
+ *
+ * Always 0 (Success)
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+
+}
