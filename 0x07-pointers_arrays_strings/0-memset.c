cd6e6d55198a00989ebaace7504f44950a617218
judith <judithuchechi62@gmail.com>
2022-09-26 13:23:29 -0700

    Memory manipulation

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * char *_memset - fills memory with a constant byte
+ * @s: pointer
+ * @b: constant byte
+ * @n: bytes of memory area
+ *
+ * Return: pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+	{
+		s[k] = b;
+	}
+
+	return (s);
+}
+
