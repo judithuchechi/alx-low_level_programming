e9b2c6aff7c9b54be7c620d74bfe1c704f5c21a0
judith <judithuchechi62@gmail.com>
2022-09-30 14:07:21 -0700

    correction

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
