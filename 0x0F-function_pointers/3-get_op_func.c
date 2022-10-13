0d6f4e0bd0daf1341a2b547dc75f0384fe9cd3fd
judith <judithuchechi62@gmail.com>
2022-10-13 13:22:19 -0700

    correction2

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <stddef.h>
+
 /**
  * get_op_func - Selects the correct function to perform
  *               the operation asked by the user.
@@ -21,12 +21,8 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
-	{
-		if (*s == *ops[i].op)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
 
-	return (NULL);
+	return (ops[i].f);
 }
