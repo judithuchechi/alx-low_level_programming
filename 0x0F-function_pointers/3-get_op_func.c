27365d99a4e0c9cf695903868e6e670196755af3
judith <judithuchechi62@gmail.com>
2022-10-12 21:42:15 -0700

    cirrection

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,18 +21,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
-
-	return (ops[i].f);
-}
-	/*
-	 * while (i < 5)
+	while (i < 5)
 	{
 		if (*s == *ops[i].op)
 			return (ops[i].f);
 		i++;
 	}
 	return (NULL);
-	*/
+}
+	
 
